f36c84d71134824df77c4ffba3b3f884c7cc00d5
Valdas Germanauskas <vgrmmail@gmail.com>
2019-10-06 15:37:23 +0300

    init

---
+++ featherink/ClientApp/src/components/Login.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+
+export class Login extends Component {
+    static displayName = Login.name;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+
+                <p>This is a simple example of a React component.</p>
+                <p aria-live="polite">Current count:</p>
+
+            </div>
+        );
+    }
+}
